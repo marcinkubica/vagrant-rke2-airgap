2efbb7232303be49b2ae88caa4af893b56567a1f
Marcin Kubica <marcin.kubica@engineerbetter.com>
2023-02-20 16:37:50 +0000

    add alias and node waits after provision

--- vagrant.sh
+++ vagrant.sh
@@ -10,6 +10,9 @@ if [ ! "${NO_TRIGGERS:-}" ]; then
     printf "${pp}# running rke2-ansible on a controller VM${nc}\n"
     vagrant ssh controller -c 'cd ./rke2-ansible && ansible-playbook site.yml -v'
 
+    printf "${pp}# accessing master-01 to check nodes status ready${nc}\n"
+    vagrant ssh master-01 -c 'sudo /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml wait node --all --for condition=ready --timeout=300s'
+
     printf "${pp}## FINISHED ##${nc}\n"
 else
     printf "${pp}NO_TRIGGERS is set. Skipping provisioning.${nc}\n"
