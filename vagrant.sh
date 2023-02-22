72987e67df7c93512b7dfc9e1ac42a2f14f91111
Marcin Kubica <marcin.kubica@engineerbetter.com>
2023-02-22 16:38:02 +0000

    Fix shellcheck SC2059
    https://www.shellcheck.net/wiki/SC2059

--- vagrant.sh
+++ vagrant.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 pp='\033[0;35m' #purple
-nc='\033[0m'    #no color
+nc='\033[0m\n'  #no color + newline
 
 if [ "${VAGRANT_TRIGGER:-}" == "false" ]
   then
-    printf "${pp}VAGRANT_TRIGGER is false. Skipping provisioning.${nc}\n"
+    printf "%bVAGRANT_TRIGGER is false. Skipping provisioning.%b" "${pp}" "${nc}"
   else
-    printf "${pp}# prepping vagrant VMs${nc}\n"
+    printf "%b# prepping vagrant VMs%b" "${pp}" "${nc}"
     ansible-playbook vagrant-site.yaml
 
-    printf "${pp}# installing rke2-ansible via controller VM${nc}\n"
+    printf "%b# installing rke2-ansible via controller VM%b" "${pp}" "${nc}"
     vagrant ssh controller -c 'cd ./rke2-ansible && ansible-playbook site.yml -v'
 
-    printf "${pp}# saving master-01 kubeconfig to /tmp/rke2-ansible.yaml${nc}\n"
+    printf "%b# saving master-01 kubeconfig to /tmp/rke2-ansible.yaml%b" "${pp}" "${nc}"
     ansible ,master-01 -b -m fetch -a 'src=/etc/rancher/rke2/rke2.yaml dest=/tmp/rke2-ansible.yaml flat=yes'
 
-    printf "${pp}# accessing master-01 to check nodes status ready${nc}\n"
+    printf "%b# accessing master-01 to check nodes status ready%b" "${pp}" "${nc}"
     vagrant ssh master-01 -c 'sudo /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml wait node --all --for condition=ready --timeout=300s'
 
-    printf "${pp}## FINISHED ##${nc}\n"
+    printf "%b## FINISHED ##%b" "${pp}" "${nc}"
 fi
